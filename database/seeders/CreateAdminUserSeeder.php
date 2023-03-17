b491a66fbd81da20b6f8ca17c76573d5be92cc99
Vinyug <vincentguyomarch89@gmail.com>
2023-03-17 18:32:03 +0100

    Seeder : PermissionTable and CreateAdminUser

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'firstname' => 'admin', 
+            'lastname' => 'admin', 
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('password'),
+        ]);
+    
+        $role = Role::create(['name' => 'Admin']);
+     
+        $permissions = Permission::pluck('id','id');
+   
+        $role->syncPermissions($permissions);
+     
+        $user->assignRole([$role->id]);
+
+        // Attach permissions to user
+        $userPermissions = Permission::all();
+        foreach ($userPermissions as $permission) {
+            $user->givePermissionTo($permission);
+        }
+    }
+}
