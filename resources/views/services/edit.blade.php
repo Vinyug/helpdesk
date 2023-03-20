1bb3049ece4037916ad9c09a566c853e7ee9cb43
Vinyug <vincentguyomarch89@gmail.com>
2023-03-20 11:23:16 +0100

    Service : views index, create, edit

---
+++ resources/views/services/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container mb-16 mx-auto sm:px-4">
+        <div class="flex flex-wrap">
+            <div class="lg:w-full pr-4 pl-4 mt-5">
+                <div class="pull-left mb-2">
+                    <h2 class="font-share-tech mt-8 mb-12 text-4xl">Modifier un service</h2>
+                </div>
+                <div class="pull-right my-3">
+                    <a class="btn-blue" href="{{ route('services.index') }}"> Retour</a>
+                </div>
+            </div>
+        </div>
+
+        @if(session('status'))
+        <div class="custom-status">
+            {{ session('status') }}
+        </div>
+        @endif
+
+        <form action="{{ route('services.update',$listing->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+
+            <div class="grid sm:grid-cols-2 gap-4 p-4">    
+                <div class="col-span-full">
+                    <label for="service" class="custom-label">Nom du service : <span class="text-red-600 font-bold">*</span></label>
+                    <input type="text" name="service" id="service" class="custom-input" placeholder="Saisir le nom du service" value="{{ $listing->service }}" >
+                    @error('service')
+                    <div class="custom-error">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="block col-span-full text-red-600 mb-5 ml-4">* les champs obligatoires</div>
+
+                <div class="col-span-full">
+                    <button type="submit" class="btn-orange">Modifier</button>
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
---
+++ resources/views/services/index.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container mb-16 mx-auto sm:px-4">
+        <div class="flex flex-wrap">
+            <div class="lg:w-full pr-4 pl-4 mt-5">
+                <div class="pull-left mb-2">
+                    <h2 class="font-share-tech mt-8 mb-12 text-4xl">Liste des services</h2>
+                </div>
+                <div class="pull-right my-3">
+                    @can('service-create')
+                    <a class="btn-green" href="{{ route('services.create') }}"> Créer un service</a>
+                    @endcan
+                </div>
+            </div>
+        </div>
+        @if ($message = Session::get('success'))
+            <div class="custom-status">
+                <p>{{ $message }}</p>
+            </div>
+        @endif
+        
+        <table class="min-w-full text-left">
+            <thead class="border-b dark:border-neutral-500">
+                <tr>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th width="280px">Action</th>
+                </tr>
+            
+            </thead>
+            <tbody>
+                @foreach ($listings as $listing)
+                <tr class="h-12 border-b dark:border-neutral-500">
+                    <td>{{ ++$i }}</td>
+                    <td>{{ $listing->service }}</td>
+                    <td>
+                        <div class="flex justify-center">               
+                            @can('service-edit')
+                                <a class="btn-blue" href="{{ route('services.edit', $listing->id) }}">Edit</a>
+                            @endcan
+    
+                            @can('service-delete')
+                            <form action="{{ route('services.destroy', $listing->id) }}" method="Post">
+                                @csrf
+                                @method('DELETE')
+                                
+                                <button type="submit" class="btn-red">Delete</button>
+                            </form>
+                            @endcan
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        
+        {!! $listings->render() !!}
+    </div>
+@endsection
