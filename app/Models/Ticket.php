6e169613b62fe960868b9b37752493ff260f8e3c
Vinyug <vincentguyomarch89@gmail.com>
2023-03-21 16:00:37 +0100

    Model : Ticket and User

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'time_id',
+        'ticket_number', 
+        'uuid',
+        'state',
+        'service', 
+        'visibility', 
+    ];
+
+    // allow to transmit uuid in URL (instead of id by default)
+    public function getRouteKeyName(): string
+    {
+        return 'uuid';
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Company::class);
     }
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
