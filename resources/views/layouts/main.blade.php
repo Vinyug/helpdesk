18e89f9e6dce1eaa0c1dda253d690ff034a950f1
Vinyug <vincentguyomarch89@gmail.com>
2023-03-22 19:22:03 +0100

    main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,7 +35,7 @@
         @include('includes.footer')
         
         @livewireScripts
-        {{-- <script src="{{ asset('assets/js/script.js') }}"></script> --}}
+        <script src="{{ asset('assets/js/script.js') }}"></script>
     </body>
     </html>
     
