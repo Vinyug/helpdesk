56a6968d9d7528ff369ca3790113ab7ce017fdeb
Vinyug <vincentguyomarch89@gmail.com>
2023-03-24 08:48:55 +0100

    npm build

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -27,7 +27,6 @@
 
         {{-- Header --}}
         <div class="flex flex-wrap mb-12">
-            test
             <div class="lg:w-full pr-4 pl-4 mt-5">
                 <p class="mb-2"><span class="font-bold">Sujet : </span>{{ $ticket->subject }}</p>
                 <p class="mb-2"><span class="font-bold">Service : </span>{{ $ticket->service }}</p>
@@ -35,7 +34,8 @@
                 <p class="mb-2"><span class="font-bold">Crée par : </span>{{ $ticket->user->firstname }} {{ $ticket->user->lastname }}</p>
                 <p class="mb-2"><span class="font-bold">État : </span>{{ $ticket->state }}</p>
                 <div>
-                    <div class="group inline relative mb-2">
+                    <div class="gro
+                    up inline relative mb-2">
                         <span class="font-bold cursor-help hover:text-custom-blue mb-2">Visibilité : </span>
                         <div class="hidden w-[220px] sm:w-[360px] group-hover:block bg-slate-50 transition-opacity p-2 text-sm italic rounded-sm border border-gray-300 absolute mb-4">
                             <p><span class="font-bold">Publique : </span>visible par tous les membres de l'entreprise.</p>
