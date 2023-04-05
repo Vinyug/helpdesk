db94ed7f2a3ecb010b9c6354204f83647403e03a
Vinyug <vincentguyomarch89@gmail.com>
2023-04-05 11:21:10 +0200

    vite.config.js : config to send *.svg to assets/images/

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import copy from 'rollup-plugin-copy'
 import laravel from 'laravel-vite-plugin'
 import path from 'path'
 import WindiCSS from 'vite-plugin-windicss'
@@ -6,6 +7,12 @@ import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
   plugins: [
+    copy({
+      targets: [
+        { src: 'assets/*.svg', 
+        dest: 'public/assets/images' }
+      ]
+    }),
     laravel([
       'resources/js/app.js',
       'resources/css/app.css',
