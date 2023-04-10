6433d5d73985cdfeee13978b2b405236b6f220be
Vinyug <vincentguyomarch89@gmail.com>
2023-04-10 18:49:34 +0200

    ticket : header form visibility and state npm build

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -27,22 +27,67 @@
 
         {{-- Header --}}
         <div class="flex flex-wrap mb-12">
-            <div class="lg:w-full pr-4 pl-4 mt-5">
+            <div class="w-full pr-4 pl-4 mt-5">
                 <p class="mb-2"><span class="font-bold">Sujet : </span>{{ $ticket->subject }}</p>
                 <p class="mb-2"><span class="font-bold">Service : </span>{{ $ticket->service }}</p>
                 <p class="mb-2"><span class="font-bold">N° Ticket : </span>{{ $ticket->ticket_number }}</p>
                 <p class="mb-2"><span class="font-bold">Crée par : </span>{{ $ticket->user->firstname }} {{ $ticket->user->lastname }}</p>
                 <p class="mb-2"><span class="font-bold">État : </span>{{ $ticket->state }}</p>
-                <div>
-                    <div class="group inline relative mb-2">
-                        <span class="font-bold cursor-help hover:text-custom-blue mb-2">Visibilité : </span>
-                        <div class="hidden w-[220px] sm:w-[360px] group-hover:block bg-slate-50 transition-opacity p-2 text-sm italic rounded-sm border border-gray-300 absolute mb-4">
-                            <p><span class="font-bold">Publique : </span>visible par tous les membres de l'entreprise.</p>
-                            <p><span class="font-bold">Privée : </span>visible par vous et l'administrateur entreprise.</p>
+                
+                @if(!auth()->user()->can('all-access'))
+                    <div>
+                        <div class="group inline relative mb-2">
+                            <span class="font-bold cursor-help hover:text-custom-blue mb-2">Visibilité : </span>
+                            <div class="hidden w-[220px] sm:w-[360px] group-hover:block bg-slate-50 transition-opacity p-2 text-sm italic rounded-sm border border-gray-300 absolute mb-4">
+                                <p><span class="font-bold">Publique : </span>visible par tous les membres de l'entreprise.</p>
+                                <p><span class="font-bold">Privée : </span>visible par vous et l'administrateur entreprise.</p>
+                            </div>
                         </div>
+                        <span> @if ($ticket->visibility) publique @else privée @endif</span>
                     </div>
-                    <span> @if ($ticket->visibility) publique @else privée @endif</span>
-                </div>
+                @else
+                    <form action="{{ route('tickets.update',$ticket->uuid) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+            
+                        <div class="grid sm:grid-cols-2 gap-2">  
+            
+                            <div class="col-span-full">
+                                <div class="flex items-center">
+                                    <div class="group inline relative">
+                                        <label for="visibility" class="custom-label mb-0">Visibilité :</label>
+                                        
+                                        {{-- TOOLTIP --}}
+                                        <div class="hidden w-[220px] sm:w-[410px] group-hover:block bg-slate-50 transition-opacity p-2 text-sm italic rounded-sm border border-gray-300 absolute">
+                                            <p><span class="font-bold">Publique : </span>visible par tous les membres de l'entreprise.</p>
+                                            <p><span class="font-bold">Privée (coché) : </span>visible par vous et l'administrateur entreprise.</p>
+                                        </div>
+                                    </div>
+                
+                                    <input type="checkbox" name="visibility" id="visibility" value="{{ $ticket->visibility }}" class="ml-4 border-gray-300 text-custom-blue focus:border-custom-blue focus:ring-custom-blue rounded-sm shadow-sm transition duration-300 ease-in-out" {{ old('visibility', !$ticket->visibility) ? 'checked' : '' }}>
+                                </div>
+                
+                                @error('visibility')
+                                <div class="custom-error">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            
+                            <div class="col-span-full sm:w-1/2 lg:w-1/4 flex h-8">
+                                <label for="state" class="custom-label pr-4 mb-0 whitespace-nowrap self-center">État :</label>
+                                <select class="custom-input py-0" name="state" id="state">
+                                    <option value="{{ $ticket->state }}">{{ $ticket->state }}</option>
+                                </select>
+                                @error('state')
+                                <div class="custom-error">{{ $message }}</div>
+                                @enderror
+                            </div>
+            
+                            <div class="col-span-full">
+                                <button type="submit" class="btn-comment-orange">Modifier</button>
+                            </div>
+                        </div>
+                    </form>
+                @endif
             </div>
         </div>
 
