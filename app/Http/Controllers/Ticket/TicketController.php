6bddffb37e16d0f070c10aef9003de749413ae66
Vinyug <vincentguyomarch89@gmail.com>
2023-04-12 09:20:25 +0200

    route correction ticket

--- app/Http/Controllers/Ticket/TicketController.php
+++ app/Http/Controllers/Ticket/TicketController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Ticket;
 
+use App\Http\Controllers\Controller;
 use App\Models\Comment;
 use App\Models\Company;
 use App\Models\Listing;
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Listing\StateController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RoleController;
-use App\Http\Controllers\TicketController;
+use App\Http\Controllers\Ticket\TicketController;
 use App\Http\Controllers\TimeController;
 use Illuminate\Support\Facades\Route;
 
