c0605f44077360269954511d968acb7c1e385e25
Vinyug <vincentguyomarch89@gmail.com>
2023-04-14 14:07:56 +0200

    delete-company correction

--- resources/views/livewire/modal_powergrid/delete-company.blade.php
+++ resources/views/livewire/modal_powergrid/delete-company.blade.php
@@ -7,7 +7,7 @@
     <p class="mt-1 text-sm text-custom-grey">Une fois cette entreprise supprimée, toutes ses ressources et données seront définitivement effacées.</p>
     
     @if ($company->users()->exists())
-        <p class="mt-4 text-sm font-medium text-red-600">Vous ne pouvez pas supprimer l'entreprise. Tous les employés doivent être réattribué à une nouvelle entreprise.</p>
+        <p class="mt-4 text-sm font-medium text-red-600">Vous ne pouvez pas supprimer l'entreprise. Tous les employés doivent être réattribués à une nouvelle entreprise.</p>
     @endif
 
     <div class="mt-2 flex flex-wrap justify-end">
@@ -20,7 +20,7 @@
                 {{ __('Confirmer suppression') }}
             </x-danger-button>
         @else
-            <a href="{{ route('users.index')}}" class="btn-comment-orange ml-0 mt-4 sm:ml-3 w-full sm:w-auto cursor-pointer">Liste des utilisateurs</a>
+            <a href="{{ route('users.index')}}" class="btn-solid-orange ml-0 mt-4 sm:ml-3 w-full sm:w-auto cursor-pointer">Liste des utilisateurs</a>
         @endif
     </div>
 </form>
