69522f894dd328816f6ce935f1f287b61e04cff2
Vinyug <vincentguyomarch89@gmail.com>
2023-04-19 16:42:32 +0200

    Reset password : view

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
     <x-guest-layout>
+        <p class="font-share-tech text-center text-3xl font-medium mb-4">Réinitialiser le mot de passe</p>
+
         <form method="POST" action="{{ route('password.store') }}">
             @csrf
 
@@ -17,14 +19,14 @@
 
             <!-- Password -->
             <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
+                <x-input-label for="password" :value="__('Mot de passe')" />
                 <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
 
             <!-- Confirm Password -->
             <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                <x-input-label for="password_confirmation" :value="__('Confirmation du mot de passe')" />
 
                 <x-text-input id="password_confirmation" class="block mt-1 w-full"
                                     type="password"
@@ -33,9 +35,9 @@
                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="mt-8">
                 <x-primary-button>
-                    {{ __('Reset Password') }}
+                    {{ __('Réinitialisation du mot de passe') }}
                 </x-primary-button>
             </div>
         </form>
