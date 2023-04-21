e1a0084bab673c31c7e2368b30537028bdeb99b1
Vinyug <vincentguyomarch89@gmail.com>
2023-04-21 21:06:10 +0200

    Home : prepare view and setup services

---
+++ resources/views/includes/home/services.blade.php
@@ -0,0 +1,31 @@
+<div class="relative mt-24 md:mt-36 mb-8 md:mb-16">
+
+    {{--------------------- HEADER ----------------------}}
+    <div class="text-center">
+        <h2 class="font-share-tech text-3xl sm:text-4xl md:text-5xl font-medium">Nos Services</h2>
+        <span class="absolute w-full -top-7 sm:-top-9 md:-top-16 left-0 font-share-tech uppercase text-custom-grey opacity-[3%] tracking-wide text-5xl sm:text-6xl md:text-8xl">Services</span>
+    </div>
+    
+    {{--------------------- CARD ----------------------}}
+    <div class="flex flex-col mt-16 md:mt-20 md:mx-[5%] lg:mx-[15%] xl:mx-[15%] 2xl:mx-[20%]">
+
+        {{-- card left --}}
+        <div class="flex flex-col md:self-start items-center mb-8 md:mb-12 bg-white border border-gray-200 rounded-sm shadow md:flex-row md:max-w-xl xl:max-w-3xl md:min-h-[256px]">
+            <img class="md:order-1 w-16 mt-8 md:mt-0 md:ml-10 md:mr-4" src="assets/images/img-service.png" alt="image service">
+            <div class="md:order-2 flex flex-col justify-between my-2 p-8 leading-normal md:mr-2">
+                <h3 class="font-share-tech mb-2 text-2xl font-medium">Quisque libero metus condimentum</h3>
+                <p class="mt-4">Quisque malesuada placerat nisl. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Phasellus gravida semper nisi. Fusce convallis metus id felis luctus adipiscing.</p>
+            </div>
+        </div>
+
+        {{-- card right --}}
+        <div class="flex flex-col md:self-end items-center mb-8 md:mb-12 bg-white border border-gray-200 rounded-sm shadow md:flex-row md:max-w-xl xl:max-w-3xl md:min-h-[256px]">
+            <img class="md:order-2 w-16 mt-8 md:mt-0 md:mr-10 md:ml-4" src="assets/images/img-service.png" alt="image service">
+            <div class="md:order-1 flex flex-col justify-between my-2 p-8 leading-normal md:ml-2">
+                <h3 class="font-share-tech mb-2 text-2xl font-medium">Quisque libero metus condimentum</h3>
+                <p class="mt-4">Quisque malesuada placerat nisl. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Phasellus gravida semper nisi. Fusce convallis metus id felis luctus adipiscing.</p>
+            </div>
+        </div>
+    </div>
+
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,5 +2,21 @@
 
 @section('content')
 
+    <div class="container mb-16 mx-auto px-4">
+        <div class="flex flex-col flex-wrap">
+            
+            <div>
+                @include('includes.home.services')
+            </div>
+
+            <div>
+                @include('includes.home.reviews')
+            </div>
+
+            <div>
+                @include('includes.home.contact')
+            </div>
+        </div>
+    </div>
 
 @endsection
