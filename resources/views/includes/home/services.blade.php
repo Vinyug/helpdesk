0d506fa1860a098fa162f77d1f6d29f9ee8617a5
Vinyug <vincentguyomarch89@gmail.com>
2023-04-21 23:57:22 +0200

    Home : list of services

--- resources/views/includes/home/services.blade.php
+++ resources/views/includes/home/services.blade.php
@@ -8,24 +8,35 @@
     
     {{--------------------- CARD ----------------------}}
     <div class="flex flex-col mt-16 md:mt-20 md:mx-[5%] lg:mx-[15%] xl:mx-[15%] 2xl:mx-[20%]">
+        @php
+            $counter = 0
+        @endphp
 
-        {{-- card left --}}
-        <div class="flex flex-col md:self-start items-center mb-8 md:mb-12 bg-white border border-gray-200 rounded-sm shadow md:flex-row md:max-w-xl xl:max-w-3xl md:min-h-[256px]">
-            <img class="md:order-1 w-16 mt-8 md:mt-0 md:ml-10 md:mr-4" src="assets/images/img-service.png" alt="image service">
-            <div class="md:order-2 flex flex-col justify-between my-2 p-8 leading-normal md:mr-2">
-                <h3 class="font-share-tech mb-2 text-2xl font-medium">Quisque libero metus condimentum</h3>
-                <p class="mt-4">Quisque malesuada placerat nisl. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Phasellus gravida semper nisi. Fusce convallis metus id felis luctus adipiscing.</p>
-            </div>
-        </div>
+        @foreach ($services as $service => $description)
+            @if ($counter % 2 == 0)
+                {{-- card left --}}
+                <div class="flex flex-col md:self-start items-center mb-8 md:mb-12 bg-white border border-gray-200 rounded-sm shadow md:flex-row md:max-w-xl xl:max-w-3xl md:min-h-[256px]">
+                    <img class="md:order-1 w-16 mt-8 md:mt-0 md:ml-10 md:mr-4" src="assets/images/img-service.png" alt="image service">
+                    <div class="md:order-2 flex flex-col justify-between my-2 p-8 leading-normal md:mr-2">
+                        <h3 class="font-share-tech mb-2 text-2xl font-medium">{{ $service }}</h3>
+                        <p class="mt-4">{{ $description }}</p>
+                    </div>
+                </div>
+            @else
+                {{-- card right --}}
+                <div class="flex flex-col md:self-end items-center mb-8 md:mb-12 bg-custom-light-blue text-white border border-gray-300 rounded-sm shadow shadow-gray-300 md:flex-row md:max-w-xl xl:max-w-3xl md:min-h-[256px]">
+                    <img class="md:order-2 w-16 mt-8 md:mt-0 md:mr-10 md:ml-4" style="filter: brightness(0) invert(1);" src="assets/images/img-service.png" alt="image service">
+                    <div class="md:order-1 flex flex-col justify-between my-2 p-8 leading-normal md:ml-2">
+                        <h3 class="font-share-tech mb-2 text-2xl font-medium">{{ $service }}</h3>
+                        <p class="mt-4">{{ $description }}</p>
+                    </div>
+                </div>
+            @endif
 
-        {{-- card right --}}
-        <div class="flex flex-col md:self-end items-center mb-8 md:mb-12 bg-white border border-gray-200 rounded-sm shadow md:flex-row md:max-w-xl xl:max-w-3xl md:min-h-[256px]">
-            <img class="md:order-2 w-16 mt-8 md:mt-0 md:mr-10 md:ml-4" src="assets/images/img-service.png" alt="image service">
-            <div class="md:order-1 flex flex-col justify-between my-2 p-8 leading-normal md:ml-2">
-                <h3 class="font-share-tech mb-2 text-2xl font-medium">Quisque libero metus condimentum</h3>
-                <p class="mt-4">Quisque malesuada placerat nisl. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Phasellus gravida semper nisi. Fusce convallis metus id felis luctus adipiscing.</p>
-            </div>
-        </div>
+            @php
+                $counter++;
+            @endphp
+        @endforeach
     </div>
 
 </div>
